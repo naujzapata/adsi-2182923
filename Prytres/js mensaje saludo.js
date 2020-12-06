ced5f5b6fd1b46207478fe35c7465c78c0f1df72
naujzapata <73726655+naujzapata@users.noreply.github.com>
2020-12-05 22:33:28 -0500

    Add files via upload

---
+++ Prytres/js mensaje saludo.js
@@ -0,0 +1,15 @@
+console.log ("Saludo desde el JavaScript ");
+
+/* primer funcion  en JavaScript alt + shift + A*/
+/* Funcion de escritura de JS */
+function nombreFuncion(){
+   /* Intrucciones  - Operaciones de la funcion */ 
+document.getElementById("msg1").innerHTML= "hola desde el JavaScript";
+}
+function alertaMensaje() {
+    /* let nombreVariable */
+    let mensaje = prompt("Bienvenido al sistema, por favor ingrese su nombre");
+}
+
+    
+}
---
+++ pryCuatro/js/JS condicionales.js
@@ -0,0 +1,1 @@
+console.log("Holsa desde el JavasScrit");
